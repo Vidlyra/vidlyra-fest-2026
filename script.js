8afa00e93877a183d27471e79a1bd165680ccd9a
Vidlyra <funhigh924@gmail.com>
2026-07-02 19:22:03 +0530

    script.js

--- script.js
+++ script.js
@@ -253,3 +253,31 @@ menuToggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 
 });
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const increment = Math.ceil(target / 100);
+
+        if(count < target){
+
+            counter.innerText = count + increment;
+
+            setTimeout(updateCounter,20);
+
+        }else{
+
+            counter.innerText = target;
+
+        }
+
+    };
+
+    updateCounter();
+
+});
