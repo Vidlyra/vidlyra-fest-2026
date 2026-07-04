ce90f684737fa688f1e3c680019b7f24381dce50
Vidlyra <funhigh924@gmail.com>
2026-07-04 11:01:55 +0530

    signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,31 @@
+async function signUp() {
 
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const name = document.getElementById("name").value;
+
+    const { data, error } = await supabase.auth.signUp({
+
+        email: email,
+        password: password,
+
+        options:{
+            data:{
+                full_name:name
+            }
+        }
+
+    });
+
+    if(error){
+
+        document.getElementById("message").innerHTML=error.message;
+
+    }else{
+
+        document.getElementById("message").innerHTML=
+        "✅ Account created! Check your email to verify your account.";
+
+    }
+
+}
