4c7955104af4d406c67a8bffa2c99947eaa282e9
Vidlyra <funhigh924@gmail.com>
2026-07-04 14:28:37 +0530

    login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,32 @@
+async function login() {
 
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const { data, error } =
+    await window.sb.auth.signInWithPassword({
+
+        email: email,
+        password: password
+
+    });
+
+    if(error){
+
+        document.getElementById("message").innerHTML =
+        error.message;
+
+    }else{
+
+        document.getElementById("message").innerHTML =
+        "✅ Login Successful";
+
+        setTimeout(function(){
+
+            window.location.href="dashboard.html";
+
+        },1000);
+
+    }
+
+}
