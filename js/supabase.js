2e0fbbc79e371282dda4c1da2f7497adcd47e740
Vidlyra <funhigh924@gmail.com>
2026-07-04 11:11:37 +0530

    supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,7 @@
 // Replace these with your own values
-const SUPABASE_URL = https://sfehwodlybnvrbotrtyc.supabase.co/rest/v1/;
-const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmZWh3b2RseWJudnJib3RydHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMjM4NDMsImV4cCI6MjA5ODY5OTg0M30.UfW_ytOEdFi645m8vtu4aT3J5QXGzUSg4hFkquhcc10;
+const SUPABASE_URL = "https://sfehwodlybnvrbotrtyc.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmZWh3b2RseWJudnJib3RydHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMjM4NDMsImV4cCI6MjA5ODY5OTg0M30.UfW_ytOEdFi645m8vtu4aT3J5QXGzUSg4hFkquhcc10";
 
 // Create Supabase client
 const supabase = window.supabase.createClient(
