ab87b4b91a7e86ad8d91f7722a3feddd5c5cae3c
Vidlyra <funhigh924@gmail.com>
2026-07-05 18:44:14 +0530

    events.js

--- js/events.js
+++ js/events.js
@@ -34,5 +34,49 @@ async function joinEvent(eventName) {
         alert(error.message);
     } else {
         alert("🎉 Successfully joined " + eventName + "!");
+loadMyEvents();
     }
 }
+async function loadMyEvents() {
+
+    const {
+        data: { user }
+    } = await window.sb.auth.getUser();
+
+    if (!user) return;
+
+    const { data, error } = await window.sb
+        .from("event_registrations")
+        .select("*")
+        .eq("user_id", user.id);
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    let html = "";
+
+    if (!data || data.length === 0) {
+
+        html = "No events joined yet.";
+
+    } else {
+
+        data.forEach(event => {
+
+            html += `
+                <p>✅ ${event.event_name}</p>
+            `;
+
+        });
+
+        html += `
+            <hr style="margin:15px 0;">
+            <strong>Total Joined : ${data.length}</strong>
+        `;
+    }
+
+    document.getElementById("myEvents").innerHTML = html;
+}
+window.addEventListener("load", loadMyEvents);
